0ec4788deeb5a2103cd6de3c1bcc74d151f7eb84
EndriuK <157508097+EndriuK@users.noreply.github.com>
2024-08-30 11:18:45 +0200

    [UPD] update top-header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -2,29 +2,35 @@
 
 
 export default {
-    
 }
 </script>
 
-
 <template lang="">
-    <div class="app-header ">
+    <div class="app-header">
         <div class="container">
             <div class="row">
-                <div class="col-12 text-light d-flex justify-content-between">
-                    <span>Open Hours: Mon - Sat - 9:00 - 18:00</span>
-                    <span class="ms-3">+1 (305) 1234-5678 hello@example.com</span>
+                <div class="col-12 text-light d-flex justify-content-between align-items-center">
+                    <span><i class="fas fa-clock"></i>Open Hours: Mon - Sat - 9:00 - 18:00</span>
+                    <span class="ms-3">
+                        <i class="fas fa-phone"></i> +1 (305) 1234-5678 <i class="fas fa-envelope"></i> hello@example.com
+                        <i class="fab fa-facebook-f ms-3"></i>
+                        <i class="fab fa-twitter ms-3"></i>
+                        <i class="fab fa-linkedin-in ms-3"></i>
+                    </span>
                 </div>
             </div>
         </div>
     </div>
 </template>
 
-
-
 <style lang="scss" scoped>
 .app-header {
     background-color: #0D1B2A;
-  }
+    padding: 1rem;
+}
 
+.app-header i {
+    color: #ffffff;
+    font-size: 1.2rem; 
+}
 </style>
