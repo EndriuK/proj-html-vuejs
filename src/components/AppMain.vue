0149af89658af17ffdd0c2a349683b14829885a2
EndriuK <157508097+EndriuK@users.noreply.github.com>
2024-08-30 16:36:52 +0200

    [UPD] top main + btn

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -7,10 +7,41 @@ export default {
 }
 </script>
 <template lang="">
-    <div>
-        
+    <div class="main-section container-fluid py-5" >
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <h6>our bussiness area</h6>
+                    <h2>Excellence in <span>service</span></h2>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-8">
+                    <p>We are leaders in providng consultacy services with a set of cutting-edge technologies and a team of experienced and renowned professionals. These are some options that you can hire.</p>
+                </div>
+                <div class="col-4 text-end">
+                    <button class="btn btn-primary">See All</button>
+                </div>
+            </div>
+        </div>
+
     </div>
 </template>
 <style lang="scss" scoped>
     
+.main-section {
+    min-height: 500px;
+    background-color: #e2e3e7;
+}
+
+.btn {
+    background-color: #058283;
+    color: #ffffff;
+
+    &:hover {
+        background-color: #ffffff;
+        color: #058283;
+    }
+    
+}
 </style>
